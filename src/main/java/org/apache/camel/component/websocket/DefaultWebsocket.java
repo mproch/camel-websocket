9922fe1825ce0c3261f661e2dc92e4a7f8bbd4d5
Christopher Köster <ck@tobias-catify.(none)>
2011-06-15 12:55:09 +0200

    websocketStore now transient

--- src/main/java/org/apache/camel/component/websocket/DefaultWebsocket.java
+++ src/main/java/org/apache/camel/component/websocket/DefaultWebsocket.java
@@ -7,54 +7,60 @@ import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocket.OnTextMessage;
 
 public class DefaultWebsocket implements WebSocket, OnTextMessage, Serializable {
+	
+	private static final long serialVersionUID = -575701599776801400L;
+	private Connection connection;
+	private String connectionKey;
+	
+	private transient WebsocketStore store;
+	private transient WebsocketConsumer consumer;
+	
+	public DefaultWebsocket(WebsocketStore store, WebsocketConsumer consumer) {
+		this.store = store;
+		this.consumer = consumer;
+	}
+	
+	@Override
+	public void onClose(int closeCode, String message) {
+		store.remove(this);
+	}
 
-    private static final long serialVersionUID = -575701599776801400L;
-    private Connection connection;
-    private String connectionKey;
+	@Override
+	public void onOpen(Connection connection) {
+		this.connection = connection;
+		this.connectionKey = UUID.randomUUID().toString();
+		store.add(this.connectionKey, this);
+	}
 
-    private WebsocketStore store;
-    private transient WebsocketConsumer consumer;
+	@Override
+	public void onMessage(String message) {
+		this.consumer.sendExchange(this.connectionKey, message);
+	}
 
-    public DefaultWebsocket(WebsocketStore store, WebsocketConsumer consumer) {
-        this.store = store;
-        this.consumer = consumer;
-    }
+	// getters and setters
+	public Connection getConnection() {
+		return connection;
+	}
 
-    @Override
-    public void onClose(int closeCode, String message) {
-        store.remove(this);
-    }
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 
-    @Override
-    public void onOpen(Connection connection) {
-        this.connection = connection;
-        this.connectionKey = UUID.randomUUID().toString();
-        store.add(this.connectionKey, this);
-    }
+	public String getConnectionKey() {
+		return connectionKey;
+	}
 
-    @Override
-    public void onMessage(String message) {
-        if (this.consumer != null) {
-            this.consumer.sendExchange(this.connectionKey, message);
-        }
-        // consumer is not set, this is produce only websocket
-        // TODO - 06.06.2011, LK - deliver exchange to dead letter channel
-    }
+	public void setConnectionKey(String connectionKey) {
+		this.connectionKey = connectionKey;
+	}
 
-    // getters and setters
-    public Connection getConnection() {
-        return connection;
-    }
+	public void setStore(WebsocketStore store) {
+		this.store = store;
+	}
 
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
-
-    public String getConnectionKey() {
-        return connectionKey;
-    }
-
-    public void setConnectionKey(String connectionKey) {
-        this.connectionKey = connectionKey;
-    }
+	public void setConsumer(WebsocketConsumer consumer) {
+		this.consumer = consumer;
+	}
+	
 }
+
